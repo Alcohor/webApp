22b4aebd0c0ea7a07267f5516c2e1e468b5e0eaa
Alcohor <1013674928@qq.com>
2018-10-15 23:26:11 +0800

    last commit in 10/15

---
+++ src/javascripts/controllers/job_controller.js
@@ -0,0 +1,18 @@
+import job_template from '../view/job_page.html'
+
+const render = ()=>{
+    let _template = Handlebars.compile(job_template)
+    let _html = _template();
+    $('.main').html(_html);
+    swiperInit();
+}
+
+const swiperInit = ()=>{
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: true,//可选选项，自动滑动
+    })
+}
+
+export default{
+    render
+}
