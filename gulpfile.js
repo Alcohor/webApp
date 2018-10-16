bbbc93fdc4e96193185c58b987a27641f4a445df
qt670717885 <670717885@qq.com>
2018-10-16 10:56:57 +0800

    qqq

--- gulpfile.js
+++ gulpfile.js
@@ -31,9 +31,13 @@ gulp.task('copy:html', () => {
 // 处理scss
 gulp.task('compile:scss', () => {
     console.log('handle scss')
-    return gulp.src('./src/stylesheets/*.scss')
+
+    setTimeout(() =>{
+        return gulp.src('./src/stylesheets/*.scss')
             .pipe(sass(sass_config).on('error', sass.logError))
             .pipe(gulp.dest('./dist/stylesheets'));
+    },500)
+    
 })
 
 // 模块化打包js
