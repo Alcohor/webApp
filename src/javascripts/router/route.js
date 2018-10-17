8f26210c9bffd4c4c3db834c5add94bf5c48965a
qt670717885 <670717885@qq.com>
2018-10-17 23:17:13 +0800

    login.html done

--- src/javascripts/router/route.js
+++ src/javascripts/router/route.js
@@ -3,6 +3,8 @@ import job_controller from '../controllers/job_controller';
 import search_controller from '../controllers/search_controller';
 import mine_controller from '../controllers/mine_controller';
 import login_controller from '../controllers/login_controller';
+import account_controller from '../controllers/accountLogin_controller';
+
 
 //页面跳转
 const routes = {
@@ -10,6 +12,7 @@ const routes = {
     '#/search' : search_controller,
     '#/mine' : mine_controller,
     '#/login' : login_controller,
+    '#/account' : account_controller
 
 }
 
